2cb5fa7aa520e4b2ab108ad7c58432f127ffbe9a
Marek Oleksik <45736363+MarekOleksik@users.noreply.github.com>
2024-09-16 08:36:40 +0200

    added searching elements in dom to app.js

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,7 @@
 console.log('DOM');
+const comments = document.querySelector('.comments__item.comments__item--newest');
+
+if (comments) {
+    const elementsWithDataInfo = comments.querySelectorAll('[data-info]');
+    console.log('The number of elements that have the attribute data-info:', elementsWithDataInfo.length);
+} 
