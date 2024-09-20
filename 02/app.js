409e6d69e18fbbbcd5b90ede086a05edc5d8b8db
Marek Oleksik <45736363+MarekOleksik@users.noreply.github.com>
2024-09-20 07:39:03 +0200

    added Task 02

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,16 @@
-console.log('DOM');
+document.addEventListener('DOMContentLoaded', function () {
+    const links = document.querySelectorAll('a[data-url]');
+
+    links.forEach(link => {
+        const url = link.getAttribute('data-url');
+
+        if (url !== 'null') {
+            link.setAttribute('href', url);
+        } else {
+            link.style.color = 'grey';  
+            link.addEventListener('click', function (event) {
+                event.preventDefault();  
+            });
+        }
+    });
+});
